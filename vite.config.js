9f7857ff6ee857e81dc14e14e864124d1bd143fd
umairakhlaque <umairakhlaque@users.noreply.github.com>
2026-04-01 13:56:45 +0000

    🚀 Add GitHub Pages auto-deploy + fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Math-Superhero-Academy/', // This matches your repository name
+  base: '/Math-game/',
 })
